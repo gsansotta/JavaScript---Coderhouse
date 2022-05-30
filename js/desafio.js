5bf567383c2a52f419361296fd406da466a078fc
Gustavo Sansotta <sansotta.gustavo@gmail.com>
2022-05-30 00:25:24 -0300

    finalizo el algoritmo - desafio 1

--- js/desafio.js
+++ js/desafio.js
@@ -15,20 +15,24 @@ let partidosGanados = 0;
 let partidosEmpatados = 0;
 let partidosPerdidos = 0;
 
+//INGRESO DE LA CANTIDAD DE PARTIDOS
 partidosJugados = parseInt(prompt("Ingrese la cantidad de partidos Jugados, 0 si no disputo ningun partido"));
 
- while(isNaN(partidosJugados) || partidosJugados < 0){
+//VALIDACIÓN DEL DATO INGRESADO POR EL USUARIO
+while (isNaN(partidosJugados) || partidosJugados < 0) {
     partidosJugados = parseInt(prompt("INGRESO INCORRECTO. Ingrese la cantidad de partidos Jugados, 0 si no disputo ningun partido"));
- }
+}
 
+//INGRESO DEL RESULTADO DE LOS PARTIDOS LAS Nº VECES EN FUNCIÓN DE LA CANTIDAD DE PARTIDOS INGRESADOS POR EL USUARIO
 for (let i = 0; i < partidosJugados; i++) {
-    resultado = prompt("Ingrese resultado, GANO, EMPATO o PERDIO.").toUpperCase();  
-   
-    while(resultado !==ganar && resultado !==empatar && resultado !==perder){
-        resultado = prompt(" INGRESO INCORRECTO. Ingrese resultado, GANO, EMPATO o PERDIO").toUpperCase();   
+    resultado = prompt("Ingrese resultado, GANO, EMPATO o PERDIO.").toUpperCase();
+
+    //VALIDACIÓN DEL DATO INGRESADO POR EL USUARIO
+    while (resultado !== ganar && resultado !== empatar && resultado !== perder) {
+        resultado = prompt(" INGRESO INCORRECTO. Ingrese resultado, GANO, EMPATO o PERDIO").toUpperCase();
     }
-   
-   switch (resultado) {
+
+    switch (resultado) {
         case (ganar):
             puntos = puntos += ptsGanar;
             partidosGanados++;
@@ -44,11 +48,12 @@ for (let i = 0; i < partidosJugados; i++) {
     }
 }
 
+//MENSAJE EN CONSOLA AL INGRESAR 0 O AL FINALIZAR LA CARGA
 if (partidosJugados === 0) {
     console.log("No ha disputado ningun partido.")
 } else {
-    console.log("Ha finalizado la carga, disputó " + partidosJugados + " partidos y obtuvo " + puntos + " puntos.");
-    console.log(`Cantidad de victorias: ${partidosGanados}, cantida de empates: ${partidosEmpatados} y cantidad de derrotas: ${partidosPerdidos}`);
+    console.log(`Ha finalizado la carga, disputó ${partidosJugados} partidos y obtuvo ${puntos} puntos.`);
+    console.log(`Cantidad de victorias: ${partidosGanados}, cantidad de empates: ${partidosEmpatados} y cantidad de derrotas: ${partidosPerdidos}`);
 }
 
 
