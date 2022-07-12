5bae627a36164e4e2175e25d7250d17bb6598cd2
Gustavo Sansotta <sansotta.gustavo@gmail.com>
2022-07-12 19:57:15 -0300

    ajusto display desde JS y agrego librerias sweet alert y AOS

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,7 @@
 /*  NUEVA VERSIÓN DEL PROYECTO FIXTURE MUNDIAL CATAR 2022
 
- EL USUARIO SELECCIONARÁ DOS EQUIPOS POR GRUPO (LOS DOS CLASIFICADOS DE LA FASE DE GRUPOS) Y AUTOMÁTICAMENTE
- SE IRÁN ARMANDO LOS CRUCES DINAMICAMENTE HASTA LLEGAR A LA FINAL.
+EL USUARIO SELECCIONARÁ DOS EQUIPOS POR GRUPO (LOS DOS CLASIFICADOS DE LA FASE DE GRUPOS) Y AUTOMÁTICAMENTE
+SE IRÁN ARMANDO LOS CRUCES DINAMICAMENTE HASTA LLEGAR A LA FINAL.
 
 
 
@@ -17,24 +17,25 @@ MEDIANTE USO DEL DOM RENDERIZO LOS GRUPOS QUE SE MUESTRAN EN PANTALLA.
 LUEGO PODER ARMAR CORRECTAMENTE LOS CRUCES DE OCTAVOS DE FINAL SEGUN MI ESCTRUCTURA DE DESARROLLO
 
 */
+
+
 const cruces = document.getElementById('fixture')
 
 function mostrarEquipos(grupo, funcionDinamica, letraGrupo) {
 
-    let grupos = document.createElement('section')
+    let grupos = document.createElement('div')
     grupos.className = 'grupo'
     var i = 0;
 
-    grupos.innerHTML =`<h1 id ="letra-grupo">${letraGrupo}</h1> `;
-
+    grupos.innerHTML = `<h1 id ="letra-grupo">${letraGrupo}</h1> `;
+    
     for (const Equipo of grupo) {
-
+        
 
         grupos.innerHTML +=
-            ` 
-            <h1> </h1>            
-         <div class="div">
-         <div id="equipo">
+            `         
+         <div data-aos="fade-down" class="div">
+         <div class="equipo" id="${Equipo.id}">
             <img src="${Equipo.bandera}" class="img-thumbnail" alt="${Equipo.nombre}-error" >        
             <button type="button" class="btn btn-light" onclick="${funcionDinamica}('${Equipo.id}')"><p>${Equipo.nombre}</p></button>
          </div>`
@@ -80,18 +81,22 @@ function primerCruce(id) {
     let equiposGrupoB = [];
     let ajustoEleccionA = cruce1.filter(actual => actual.grupo == "A")
     let ajustoEleccionB = cruce1.filter(actual => actual.grupo == 'B')
+    
     for (const equipo of equiposClasificados) {
         if (equipo.id == id && !cruce1.includes(equipo)) {
             console.log(cruce1)
+            let ocultoEquipo = document.getElementById(equipo.id)
             if (cruce1.length < 4) {
 
                 if (ajustoEleccionA.length < 2 && equipo.grupo == "A") {
                     equiposGrupoA.push(equipo)
+                    ocultoEquipo.style.display = 'none';
 
                 }
 
                 if (ajustoEleccionB.length < 2 && equipo.grupo == "B" && ajustoEleccionA.length > 1) {
                     equiposGrupoB.push(equipo)
+                    ocultoEquipo.style.display = 'none';
 
                 }
 
@@ -102,9 +107,9 @@ function primerCruce(id) {
                         background: ' #8a1538',
                         color: 'white',
                         imageUrl: 'https://cloudfront-us-east-1.images.arcpublishing.com/sdpnoticias/XUDGIXQ4TVGZDJ7KOQ2APWMKDI.jpg',
-                        icon:'warning',
+                        icon: 'warning',
                         imageWidth: 600,
-                        imageHeight: 200,                        
+                        imageHeight: 200,
                         confirmButtonText: 'OK'
                     })
                 }
@@ -121,11 +126,8 @@ function primerCruce(id) {
     PARA LUEGO TRAERLOS COMO OBJETO CON LA FUNCION RECUPERO CRUCE*/
     cruce1.push(...equiposGrupoA)
     cruce1.push(...equiposGrupoB)
-    localStorage.setItem('cruce1', JSON.stringify(cruce1))
-
-    /* mostrarEquiposOctavos(equiposGrupoA)
-    mostrarEquiposOctavos(equiposGrupoB) */
-    cruzarEquipo(cruce1, ["A", "A", "B", "B"])
+    localStorage.setItem('cruce1', JSON.stringify(cruce1))  
+    cruzarEquipo(cruce1, ["A", "A", "B", "B"])    
 }
 
 
@@ -153,25 +155,28 @@ function segundoCruce(id) {
     for (const equipo of equiposClasificados) {
         if (equipo.id == id && !cruce2.includes(equipo)) {
             console.log(cruce2)
-            if (cruce2.length < 4) {
+            let ocultoEquipo = document.getElementById(equipo.id)
+            if (cruce2.length < 4) {              
 
                 if (ajustoEleccionC.length < 2 && equipo.grupo == "C") {
                     equiposGrupoC.push(equipo)
+                    ocultoEquipo.style.display = 'none';
 
                 }
                 if (ajustoEleccionD.length < 2 && equipo.grupo == "D" && ajustoEleccionC.length > 1) {
                     equiposGrupoD.push(equipo)
+                    ocultoEquipo.style.display = 'none';
                 }
 
                 if (equipo.grupo == "D" && ajustoEleccionC.length < 2) {
                     Swal.fire({
-                        title: 'Primero debes elegir los dos equipos del grupo C',                       
+                        title: 'Primero debes elegir los dos equipos del grupo C',
                         background: ' #8a1538',
                         color: 'white',
                         imageUrl: 'https://cloudfront-us-east-1.images.arcpublishing.com/sdpnoticias/XUDGIXQ4TVGZDJ7KOQ2APWMKDI.jpg',
-                        icon:'warning',
+                        icon: 'warning',
                         imageWidth: 600,
-                        imageHeight: 200,                        
+                        imageHeight: 200,
                         confirmButtonText: 'OK'
                     })
                 }
@@ -185,9 +190,6 @@ function segundoCruce(id) {
     cruce2.push(...equiposGrupoC)
     cruce2.push(...equiposGrupoD)
     localStorage.setItem('cruce2', JSON.stringify(cruce2))
-
-    /*  mostrarEquiposOctavos(equiposGrupoC)
-     mostrarEquiposOctavos(equiposGrupoD) */
     cruzarEquipo(cruce2, ["C", "C", "D", "D"])
 }
 const recuperoCruceDos = () => {
@@ -214,14 +216,17 @@ function tercerCruce(id) {
     for (const equipo of equiposClasificados) {
         if (equipo.id == id && !cruce3.includes(equipo)) {
             console.log(cruce3)
+            let ocultoEquipo = document.getElementById(equipo.id)
             if (cruce3.length < 4) {
 
                 if (ajustoEleccionE.length < 2 && equipo.grupo == "E") {
                     equiposGrupoE.push(equipo)
+                    ocultoEquipo.style.display = 'none';
 
                 }
                 if (ajustoEleccionF.length < 2 && equipo.grupo == "F" && ajustoEleccionE.length > 1) {
                     equiposGrupoF.push(equipo)
+                    ocultoEquipo.style.display = 'none';
                 }
 
                 if (equipo.grupo == "F" && ajustoEleccionE.length < 2) {
@@ -230,9 +235,9 @@ function tercerCruce(id) {
                         background: ' #8a1538',
                         color: 'white',
                         imageUrl: 'https://cloudfront-us-east-1.images.arcpublishing.com/sdpnoticias/XUDGIXQ4TVGZDJ7KOQ2APWMKDI.jpg',
-                        icon:'warning',
+                        icon: 'warning',
                         imageWidth: 600,
-                        imageHeight: 200,                        
+                        imageHeight: 200,
                         confirmButtonText: 'OK'
                     })
                 }
@@ -244,10 +249,7 @@ function tercerCruce(id) {
 
     cruce3.push(...equiposGrupoE)
     cruce3.push(...equiposGrupoF)
-    localStorage.setItem('cruce3', JSON.stringify(cruce3))
-
-    /*  mostrarEquiposOctavos(equiposGrupoE)
-     mostrarEquiposOctavos(equiposGrupoF) */
+    localStorage.setItem('cruce3', JSON.stringify(cruce3)) 
     cruzarEquipo(cruce3, ["E", "E", "F", "F"])
 }
 const recuperoCruceTres = () => {
@@ -272,14 +274,17 @@ function cuartoCruce(id) {
     for (const equipo of equiposClasificados) {
         if (equipo.id == id && !cruce4.includes(equipo)) {
             console.log(cruce4)
+            let ocultoEquipo = document.getElementById(equipo.id)
             if (cruce4.length < 4) {
 
                 if (ajustoEleccionG.length < 2 && equipo.grupo == "G") {
                     equiposGrupoG.push(equipo)
+                    ocultoEquipo.style.display = 'none';
 
                 }
                 if (ajustoEleccionH.length < 2 && equipo.grupo == "H" && ajustoEleccionG.length > 1) {
                     equiposGrupoH.push(equipo)
+                    ocultoEquipo.style.display = 'none';
                 }
 
                 if (equipo.grupo == "H" && ajustoEleccionG.length < 2) {
@@ -288,26 +293,35 @@ function cuartoCruce(id) {
                         background: ' #8a1538',
                         color: 'white',
                         imageUrl: 'https://cloudfront-us-east-1.images.arcpublishing.com/sdpnoticias/XUDGIXQ4TVGZDJ7KOQ2APWMKDI.jpg',
-                        icon:'warning',
+                        icon: 'warning',
                         imageWidth: 600,
-                        imageHeight: 200,                        
+                        imageHeight: 200,
                         confirmButtonText: 'OK'
                     })
                 }
 
+
+                
+                
             }
         }
-
+        
     }
-
+       
     cruce4.push(...equiposGrupoG)
     cruce4.push(...equiposGrupoH)
-    localStorage.setItem('cruce4', JSON.stringify(cruce4))
-
-    /*   mostrarEquiposOctavos(equiposGrupoG)
-      mostrarEquiposOctavos(equiposGrupoH) */
+    localStorage.setItem('cruce4', JSON.stringify(cruce4))   
     cruzarEquipo(cruce4, ["G", "G", "H", "H"])
+    if(cruce4.length == 4){
+        let ocultoGrupos = document.getElementById('fixture')
+       ocultoGrupos.style.display = 'none'; 
+       let acomodoCruces =  document.getElementById('main-general')
+       acomodoCruces.style.display = 'block'; 
+    }
+
+    
 }
+
 const recuperoCruceCuatro = () => {
     let cruceCuatroRecuperado = JSON.parse(localStorage.getItem("cruce4"));
     console.log(cruceCuatroRecuperado)
@@ -323,61 +337,100 @@ console.log(JSON.parse(localStorage.getItem('cruce4')))
 /* FUNCION PARA ARMAR DINAMICAMENTE LOS CRUCES DE OCTAVOS DE FINAL */
 
 function cruzarEquipo(cruce, ubicacion) {
+    
     const primeroA = document.getElementById(`1${ubicacion[0]}`);
     const segundoA = document.getElementById(`2${ubicacion[1]}`);
     const primeroB = document.getElementById(`1${ubicacion[2]}`)
     const segundoB = document.getElementById(`2${ubicacion[3]}`)
 
-
-    primeroA.innerHTML = `          
-    <div class="div">
-    <div id="equipo" >
-    <img src="${cruce[0]['bandera']}" class="img-thumbnail" alt="${cruce[0]['nombre']}-error" >        
-    <button type="button" class="btn btn-light" onclick="('${cruce[0]['id']}')"><p>${cruce[0]['nombre']}</p></button>
-    </div>`
-    /* btn = document.getElementsByTagName("button")
-    btn.addEventListener('click', () => {
    
-        Swal.fire({
-            title: 'Está seguro de eliminar el producto?',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonText: 'Sí, seguro',
-            cancelButtonText: 'No, no quiero'
-        }).then((result) => {
-    
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: 'Borrado!',
-                    icon: 'success',
-                    text: 'El archivo ha sido borrado'
-                })
-            }
-        })
-    }) */
+    primeroA.innerHTML =` 
 
+    
+    <div class="team" id = "${cruce[0]['nombre']}">
+    <img src="${cruce[0]['bandera']}" class="img-thumbnail" alt="${cruce[0]['nombre']}-error" >        
+    <button type="button" class="btn btn-light" onclick="('${cruce[0]['id']}')"><p>${cruce[0]['nombre']}</p></button>   
+    </div>  `
 
     segundoA.innerHTML = `          
-    <div class="div">
-    <div id="equipo">
+   
+    <div class="team">
     <img src="${cruce[1]['bandera']}" class="img-thumbnail" alt="${cruce[1]['nombre']}-error" >        
     <button type="button" class="btn btn-light" onclick="('${cruce[1]['id']}')"><p>${cruce[1]['nombre']}</p></button>
     </div>`
 
     primeroB.innerHTML = `          
-    <div class="div">
-    <div id="equipo">
+    
+    <div class="team">
     <img src="${cruce[2]['bandera']}" class="img-thumbnail" alt="${cruce[2]['nombre']}-error" >        
     <button type="button" class="btn btn-light" onclick="('${cruce[2]['id']}')"><p>${cruce[2]['nombre']}</p></button>
     </div>`
 
     segundoB.innerHTML = `          
-    <div class="div">
-    <div id="equipo">
+   
+    <div class="team">
     <img src="${cruce[3]['bandera']}" class="img-thumbnail" alt="${cruce[3]['nombre']}-error" >        
-    <button type="button" class="btn btn-light" onclick="('${cruce[3]['id']}')"><p>${cruce[3]['nombre']}</p></button>
+    <button type="button" class="btn btn-light" onclick="cruceCuartos('${cruce[3]['id']}')"><p>${cruce[3]['nombre']}</p></button>
     </div>`
 
+  
 }
+ 
+
+/*  function cruceCuartos(id) {
+    
+    let cuartos1 = [];
+    let cuartos2 = [];
+    let ajustoEleccionA = cruce1.filter(actual => actual.grupo == "A")
+    let ajustoEleccionB = cruce1.filter(actual => actual.grupo == 'B')
+    
+    for (const equipo of cruce1) {
+        if (equipo.id == id && !cruce1.includes(equipo)) {
+            console.log(cuartos1)
+          
+            if (cruce1.length < 4) {
+
+                if (ajustoEleccionA.length < 2 && equipo.grupo == "A") {
+                    cuartos1.push(equipo)
+                
+
+                }
+
+                if (ajustoEleccionB.length < 2 && equipo.grupo == "B" && ajustoEleccionA.length > 1) {
+                    cuartos2.push(equipo)
+                    ocultoEquipo.style.display = 'none';
+
+                }
 
 
+                if (equipo.grupo == "B" && ajustoEleccionA.length < 2) {
+                    Swal.fire({
+                        title: 'Primero debes elegir los dos equipos del grupo A',
+                        background: ' #8a1538',
+                        color: 'white',
+                        imageUrl: 'https://cloudfront-us-east-1.images.arcpublishing.com/sdpnoticias/XUDGIXQ4TVGZDJ7KOQ2APWMKDI.jpg',
+                        icon: 'warning',
+                        imageWidth: 600,
+                        imageHeight: 200,
+                        confirmButtonText: 'OK'
+                    })
+                }
+
+            }
+
+        }
+    }
+
+
+
+
+    /* CUANDO SALGO DEL FOR OF CON LOS 4 QEQUIPOS, LOS PUSHEO AL ARRAY CRUCE1 Y LO SETEO EN EL LOCAL STORAGE PARA 
+    PARA LUEGO TRAERLOS COMO OBJETO CON LA FUNCION RECUPERO CRUCE*/
+ /*    cruce1.push(...equiposGrupoA)
+    cruce1.push(...equiposGrupoB)
+    localStorage.setItem('cruce1', JSON.stringify(cruce1)) */
+
+    /* mostrarEquiposOctavos(equiposGrupoA)
+    mostrarEquiposOctavos(equiposGrupoB) */
+    /* cruzarEquipo(cruce1, ["A", "A", "B", "B"])     */
+/* } */
